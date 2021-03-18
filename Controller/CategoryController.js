6da3749ba6e1b4fca8d0714c1eefd651e291f4a1
ngthienkhang <74594915+ngthienkhang@users.noreply.github.com>
2021-03-18 22:56:17 +0700

    Cata va Manu

--- Controller/CategoryController.js
+++ Controller/CategoryController.js
@@ -27,6 +27,118 @@ class CategoryController {
             })
         }
     }
-
+    static async getCatagoryController(req,res,next){
+        try{
+            const data = await querry("Category").select();
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                data
+            })
+        }
+        catch(e){
+            console.log(e);
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"getcatagory failed"
+                },
+                result:null
+            })
+        }
+    }
+    static async paginatingCatagoryController(req,res,next){
+        try{
+            const {page} = req.params;
+            const product = 12;
+            let startIndex = (page - 1)*product;
+            let endIndex = page * product;
+            const productList = await querry("Catagory").select();
+            const list = productList.slice(startIndex,endIndex);
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                data:list
+            })
+        }
+        catch(e){
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"paginating failed"
+                },
+                result:null
+            })
+        }
+    }
+    static async getProductByIdController(req,res,next){
+        try{
+            const {id} = req.params;
+            const product = await querry("Catgory").where("idCatagory",id).select().first()
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                data:product
+            })
+        }
+        catch(e){
+            console.log(e);
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"get product failed"
+                },
+                result:null
+            })
+        }
+    }
+    static async deleteController(req,res,next){
+        try{
+            const {id} = req.params;
+            await querry("Catagory").where("idCatagory",id).del();
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                result:"Deleted"
+            })
+        }
+        catch(e){
+            console.log(e);
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"delete product failed"
+                },
+                result:null
+            })
+        }
+    }
+    static async updateProductController(req,res,next){
+        try{
+            const {id} = req.params;
+            const{name} = req.body;
+            await querry("Catagory").where("idCatagory",id).update({name});
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                result:"Updated"
+            })
+        }
+        catch(e){
+            console.log(e);
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"update product failed"
+                },
+                result:null
+            })
+        }
+    }
 }
 module.exports = CategoryController;
--- Controller/ManufactorController.js
+++ Controller/ManufactorController.js
@@ -28,5 +28,97 @@ class ManufactorController{
             })
         }
     }
+    static async paginatingProductController(req,res,next){
+        try{
+            const {page} = req.params;
+            const product = 12;
+            let startIndex = (page - 1)*product;
+            let endIndex = page * product;
+            const productList = await querry("Manufactor").select();
+            const list = productList.slice(startIndex,endIndex);
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                data:list
+            })
+        }
+        catch(e){
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"paginating failed"
+                },
+                result:null
+            })
+        }
+    }
+    static async getProductByIdController(req,res,next){
+        try{
+            const {id} = req.params;
+            const product = await querry("Manufactor").where("idManufactor",id).select().first()
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                data:product
+            })
+        }
+        catch(e){
+            console.log(e);
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"get product failed"
+                },
+                result:null
+            })
+        }
+    }
+    static async deleteController(req,res,next){
+        try{
+            const {id} = req.params;
+            await querry("Manufactor").where("idManufactor",id).del();
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                result:"Deleted"
+            })
+        }
+        catch(e){
+            console.log(e);
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"delete product failed"
+                },
+                result:null
+            })
+        }
+    }
+    static async updateProductController(req,res,next){
+        try{
+            const {id} = req.params;
+            const{name} = req.body;
+            await querry("Manufactor").where("idManufactor",id).update({name});
+            res.status(200).json({
+                status:"SUCCESS",
+                error:null,
+                result:"Updated"
+            })
+        }
+        catch(e){
+            console.log(e);
+            res.status(400).json({
+                status:"FAILED",
+                error:{
+                    code:1000,
+                    message:"update product failed"
+                },
+                result:null
+            })
+        }
+    }
 }
 module.exports = ManufactorController
