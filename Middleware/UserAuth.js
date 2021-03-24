169528c8c3dc4cf60ec0f61379a424b0c538f2fc
Thinh Nguyen <role1thn@gmail.com>
2021-03-24 14:46:44 +0700

    connect db

---
+++ Middleware/UserAuth.js
@@ -0,0 +1,34 @@
+const e = require("express");
+const jwt = require("jsonwebtoken");
+const querryBuilder = require("../Config/Database");
+const JWT_SECRET_KEY= process.env.JWT_SECRET_KEY;
+module.exports = async(req,res,next) =>{
+    try{
+        if(req.header("Authorization")){
+            const token = req.header("Authorization").replace("Bearer ","");
+            if(token){
+                const verifyToken =await jwt.verify(token,JWT_SECRET_KEY);
+            
+                if(verifyToken){
+                    const {idUser} = verifyToken
+                
+                    const user = await querryBuilder("Customer").where("idUser",idUser).first();
+                    const {fullname,license,mail,phoneNum,birth,avatar,cmnd,password,created_at} = JSON.parse(JSON.stringify(user));
+                    next();
+                }
+            }
+            
+            }else{
+                res.status(400).json({
+                    message:"You need account to get next action"
+                })
+        }
+    }catch(e){
+        console.log(e)
+        res.status(400).JSON({
+            message:"You need account to get next action"
+        })
+    }
+    
+
+}
--- Route/Vehicle.js
+++ Route/Vehicle.js
@@ -1,7 +1,8 @@
 const route = require("express").Router();
+const UserAuth = require("../Middleware/UserAuth");
 const VehicleController = require("../Controller/VehicleController");
 route.post("/",VehicleController.addVehicleController);
-route.get("/",VehicleController.getProductController);
+route.get("/",UserAuth,VehicleController.getProductController);
 route.get("/:page",VehicleController.paginatingProductController)
 route.get("/detail/:id",VehicleController.getProductByIdController)
 route.delete("/:id",VehicleController.deleteController)
