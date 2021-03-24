87890f86344fc7afa59a4b155f9dfbb6872c6ce2
Thinh Nguyen <role1thn@gmail.com>
2021-03-24 16:38:59 +0700

    remove middleware

--- Route/Vehicle.js
+++ Route/Vehicle.js
@@ -2,7 +2,7 @@ const route = require("express").Router();
 const UserAuth = require("../Middleware/UserAuth");
 const VehicleController = require("../Controller/VehicleController");
 route.post("/",VehicleController.addVehicleController);
-route.get("/",UserAuth,VehicleController.getProductController);
+route.get("/",VehicleController.getProductController);
 route.get("/:page",VehicleController.paginatingProductController)
 route.get("/detail/:id",VehicleController.getProductByIdController)
 route.delete("/:id",VehicleController.deleteController)
