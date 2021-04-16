b99bcfa2cf0df9429967aeaff769abe70253a619
caibanThinh200 <67794875+caibanThinh200@users.noreply.github.com>
2021-04-16 14:04:57 +0700

    update heroku

--- Controller/VehicleController.js
+++ Controller/VehicleController.js
@@ -42,7 +42,7 @@ class VehicleController {
     }
     static async getProductController(req, res, next) {
         try {
-            const data = await querry("Vehicle").select().orderBy("created-at","desc");
+            const data = await querry("Vehicle").select().orderBy("created_at","desc");
             res.status(200).json({
                 status: "SUCCESS",
                 error: null,
