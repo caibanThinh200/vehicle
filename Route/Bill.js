39c5d3a6d4c2a2afff073051414fffb8da418ecb
Nguyễn Quốc Thịnh <thinh.nguyen@joyvn.vn>
2021-06-18 18:06:09 +0700

    oke

--- Route/Bill.js
+++ Route/Bill.js
@@ -5,8 +5,6 @@ route.post("/", BillController.addBillController);
 route.get("/",BillController.getBillController);
 route.get("/user", BillController.getBillByIdUserController);
 route.delete("/:id", BillController.deleteBillController);
-route.get("/:page", BillController.paginatingBillController);
-route.get("/alert/:id", BillController.alertDeleveringBill);
 route.put("/:id", BillController.updateBillStatusController);
 route.put("/admin/confirm/:id",BillController.adminBillConfirmController);
 route.get("/endTime/:id", BillController.resolveEndTimeController);
@@ -14,5 +12,7 @@ route.delete("/:id", BillController.deleteBillController);
 route.post("/KPI", BillController.AddNewMonthKPIController);
 route.get("/KPI", BillController.GetKPIOfYearByAdminController);
 route.get("/:id", BillController.getBillByIdController);
+route.get("/:page", BillController.paginatingBillController);
+route.get("/alert/:id", BillController.alertDeleveringBill);
 
 module.exports = route
